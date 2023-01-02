e7cf5509069b0f3837e2b8c7ab120a6a233db327
majesia <Wiktoria.majewska02@gmail.com>
2023-01-02 22:17:20 +0100

    canva-naprawienie błędu rysowania figur poza polem

--- src/Canvas.java
+++ src/Canvas.java
@@ -9,17 +9,16 @@ import java.awt.event.MouseListener;
 
 public class Canvas extends JFrame implements ActionListener {
     JButton bChangeColor, lCurrentColor, bClear, bExit;
-    JRadioButton rbCircle, rbTriangle,rbSquare;
+    JRadioButton rbCircle,rbSquare;
     int xMouse,yMouse, size, number;
-    JPanel  pCanva;
+    JPanel  pCanva, pButtons;
     Color currentColor;
     JSlider sSize;
+
     MouseListener mouseListener= new MouseListener() {
         @Override
         public void mouseClicked(MouseEvent e) {
-            if((300<=e.getX() && e.getX()<=1200) && (0<=e.getY() && e.getY()<=800)){
-                drawFigure(e.getX(),e.getY(),size);
-            }
+            drawFigure(e.getX(), e.getY(), size);
         }
 
         @Override
@@ -46,99 +45,89 @@ public class Canvas extends JFrame implements ActionListener {
 
 
     Canvas(){
-        setSize(1200,800);
+        setSize(1600,900);
         setTitle("Canva");
         setLayout(null);
         getContentPane().setBackground(new Color(160,200,100));
 
+        pButtons= new JPanel();
+        pButtons.setLayout(null);
+        pButtons.setBackground(Color.ORANGE);
+        pButtons.setBounds(0,0,299,900);
+        add(pButtons);
         lCurrentColor = new JButton();
         lCurrentColor.setBounds(50,25,200,100);
         lCurrentColor.setText("Current color");
-        //lCurrentColor.setBackground(Color.BLUE);
-        add(lCurrentColor);
+        pButtons.add(lCurrentColor);
 
         bChangeColor = new JButton("Choose a new color");
+
         bChangeColor.setBounds(50,150,200,100);
         bChangeColor.addActionListener(this);
-        add(bChangeColor);
+        pButtons.add(bChangeColor);
 
         pCanva =new JPanel();
-        pCanva.setBounds(300,0,900,800);
+        pCanva.setBounds(300,0,1300,900);
         pCanva.setBackground(Color.white);
         add(pCanva);
-        //pCanva.add(canvas);
-
-        //canvas.setBounds(300,0,900,800);
-        //canvas.setBackground(Color.GRAY);
+        pCanva.addMouseListener(mouseListener);
 
 
-        //add(canvas);
-        addMouseListener(mouseListener);
 
         rbCircle=new JRadioButton("Circle");
         rbSquare=new JRadioButton("Square");
-        rbTriangle= new JRadioButton("Triangle");
-        rbCircle.setBounds(50,300,100,50);
-        rbSquare.setBounds(50,350,100,50);
-        //rbTriangle.setBounds(50,400,100,50);
-        add(rbCircle);
-        //add(rbTriangle);
-        add(rbSquare);
-        //rbTriangle.addActionListener(this);
+        pButtons.add(rbCircle).setBounds(50,300,100,50);
+        pButtons.add(rbSquare).setBounds(50,350,100,50);
         rbCircle.addActionListener(this);
         rbSquare.addActionListener(this);
 
         ButtonGroup group= new ButtonGroup();
         group.add(rbCircle);
         group.add(rbSquare);
-        //group.add(rbTriangle);
 
-
-        sSize=new JSlider(0,0,50,5);
+        sSize=new JSlider(0,0,100,5);
         sSize.setBounds(50,450,200,50);
-        add(sSize);
+        pButtons.add(sSize);
         sSize.addChangeListener(changeListener);
 
         bClear=new JButton("Clear the canva");
         bClear.setBounds(50,550,200,100);
         bClear.addActionListener(this);
-        add(bClear);
+        pButtons.add(bClear);
 
         bExit=new JButton("Exit");
-        bExit.setBounds(50,750,200,70);
         bExit.addActionListener(this);
-        add(bExit);
+        bExit.setBounds(50,700,200,70);
+        pButtons.add(bExit);
 
     }
 
     Color getColor() {return currentColor;}
+
     void drawCenteredCircle(int x, int y, int r) {
-        Graphics g = getGraphics();
+        Graphics g = pCanva.getGraphics();
         g.setColor(getColor());
         g.fillOval(x+r/2,y+r/2,r,r);
     }
 
     void drawCenteredSquare(int x, int y, int r){
-        Graphics g = getGraphics();
+        Graphics g = pCanva.getGraphics();
         g.setColor(getColor());
         g.fillRect(x,y,r,r);
     }
-    void drawTriangle(int x,int y,int h){
-        Graphics g = getGraphics();
-        g.setColor(getColor());
-        g.fillArc(x,y,h,h,0,60 );
-    }
+
     Color changingColor(){
         JColorChooser colorChooser = new JColorChooser();
-        currentColor = JColorChooser.showDialog(null, "Pick a color...I guess", Color.black);
+        currentColor = colorChooser.showDialog(null, "Pick a color...I guess", Color.black);
         return currentColor;
     }
 
     void drawFigure(int x, int y, int r){
         if(number==1) drawCenteredCircle(x-r,y-r,r);
         if(number==2) drawCenteredSquare(x-r/2,y-r/2,r);
-        if(number==3) drawTriangle(x,y,r);
     }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
@@ -152,11 +141,11 @@ public class Canvas extends JFrame implements ActionListener {
 
         if(source==rbCircle) number=1;
         if(source==rbSquare) number=2;
-        if(source==rbTriangle) number=3;
 
         if(source==bClear){
-            pCanva.getGraphics().clearRect(0,0,900,800);
-            pCanva.setBackground(Color.white);
+            getContentPane().setBackground(new Color(160,200,100));
+            pCanva.getGraphics().clearRect(0,0,1300,900);
+            pCanva.setBackground(Color.WHITE);
         }
 
         if(source==bExit){
